b01d9419d1f0f4e5ce5492e4e1f1ff78bf6efb20
jstar920 <51071865@qq.com>
2017-07-26 08:50:21 +0800

    fix compiler error

--- sourceCode/ClusterMgtMessage/Component.h
+++ sourceCode/ClusterMgtMessage/Component.h
@@ -1,8 +1,8 @@
-#ifndef _CLUSTERMANAGEMENT_COMPONENT_H_
-#define _CLUSTERMANAGEMENT_COMPONENT_H_
+#ifndef _CLUSTERMGTMESSAGE_COMPONENT_H_
+#define _CLUSTERMGTMESSAGE_COMPONENT_H_
 #include <string>
 namespace ClusterMgtMessage
 {
 inline static std::string component(){return std::string("ClusterMgtMessage");}
 }
-#endif // _CLUSTERMANAGEMENT_COMPONENT_H_
+#endif // _CLUSTERMGTMESSAGE_COMPONENT_H_
