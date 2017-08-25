f77fc7fcde42c3f0e6f42782561bd7fd61c69606
jstar920 <51071865@qq.com>
2017-08-25 17:39:33 +0800

    Configuration Message (Maui request and response)

--- sourceCode/ConfigureMgtMessage/MauiConfigurationAcquireResponse.h
+++ sourceCode/ConfigureMgtMessage/MauiConfigurationAcquireResponse.h
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 #ifndef _CONFIGUREMGTMESSAGE_MAUICONFIGURATIONACQUIRERESPONSE_H_
 #define _CONFIGUREMGTMESSAGE_MAUICONFIGURATIONACQUIRERESPONSE_H_
+=======
+#ifndef MANUICONFIGURATIONACQUIRERESPONSE_H
+#define MANUICONFIGURATIONACQUIRERESPONSE_H
+>>>>>>> 6b14ce4... Configuration Message (Maui request and response)
 #include "IConfigureMgtMessage.h"
 #include "MauiConfig.h"
 
@@ -23,4 +28,8 @@ public:
 
 }
 
+<<<<<<< HEAD
 #endif // _CONFIGUREMGTMESSAGE_MAUICONFIGURATIONACQUIRERESPONSE_H_
+=======
+#endif // MANUICONFIGURATIONACQUIRERESPONSE_H
+>>>>>>> 6b14ce4... Configuration Message (Maui request and response)
