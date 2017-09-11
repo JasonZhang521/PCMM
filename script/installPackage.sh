e4b4c01b4878d59de2a5fddde371ff8d8d8ae275
jstar920 <51071865@qq.com>
2017-09-11 05:42:13 -0400

    add install script ongoing

---
+++ script/installPackage.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+ServerIpAddress=""
+PackagePath=""
+ImageType=""
+
+initInstallDir()
+{
+    rootDir=/opt/HongClusterMgt
+    if [ ! -d "$rootDir"]; then
+        mkdir $rootDir
+    fi
+    if [ ! -d "$rootDir/network"]; then
+        mkdir $rootDir/network
+    fi
+    if [ ! -d "$rootDir/log"]; then
+        mkdir $rootDir/log
+    fi
+    if [ ! -d "$rootDir/temp"]; then
+        mkdir $rootDir/temp
+    fi
+}
+
+help()
+{
+    echo "-n address, server address, for example: -n 192.168.1.135"
+    echo "-p path, package path, for example: -p /home/user/pcmm.tar.gz"
+    echo "-t type, image type, type = [server, client],  for example: -t server"
+}
+
+parseArgument()
+{
+    ArgumentList=$@
+    ArgumentNum=$#
+
+    if [ "$ArgumentNum" -eq 0 ]; then
+        echo "error arguments, usage as following:"
+        help
+        exit 1
+    fi
+    HelpOpt=($(echo "$ArgumentList" | grep " -h"))
+    if [ "$HelpOpt" == "" ]; then
+        ServerIpAddressOpt=($(echo "$ArgumentList" | grep "-n ")) 
+echo "$ArgumentList!"
+        echo "$ServerIpAddressOpt __________"
+        if [ "$ServerIpAddressOpt" = "" ]; then
+            echo "should have server Ip address, usage as following:"
+            help
+            exit 1
+        fi
+
+        PackagePathOpt=($(echo $ArgumentList | grep "-p ")) 
+        if [ "$PackagePathOpt" == "" ]; then
+            echo "should have package path, usage as following:"
+            help
+            exit 1
+        fi
+
+        ImageTypeOpt=($(echo $ArgumentList | grep "-t "))
+        if [ "$ImageTypeOpt" == "" ]; then
+            echo "should have image type, usage as following:"
+            help
+            exit 1
+        fi
+    fi
+
+    while getopts "n:p:t:h" arg
+    do
+        case $arg in
+            n)
+                ServerIpAddress=$OPTARG
+                ;;
+            p)
+                PackagePath=$OPTARG
+                if [ ! -f "$PackagePath"]; then
+                    echo "package $PackagePath is not exsisted!"
+                    exit 1
+                fi
+                ;;
+            t)
+                ImageType=$OPTARG
+                if [ "$ImageType" != "server" ] && [ "$ImageType" != "client" ]; then
+                    echo "image type error,  only support 'server' or 'client'!"
+                    exit 1
+                fi
+                ;;
+            h)
+                help
+                ;;
+            ?)
+                echo "unkonw argument, usage as following:"
+                help
+                exit 1
+                ;;
+            esac
+    done
+}
+
+parseArgument $@
