f7b18b3d4ed4d29464ef2b54efae80670e4c9015
Linux-Jstar <jstar@localhost.localdomain>
2017-09-21 22:53:37 +0800

    Add make package

--- script/makePackage.sh
+++ script/makePackage.sh
@@ -22,12 +22,21 @@ else
     rm -rf "$TempDir/*"
 fi
 
+GccDir=`which gcc`
+LibStd6Dir="${GccDir%/*/*}/lib64/"
+LibStd6Link="${GccDir%/*/*}/lib64/libstdc++.so.6"
+LibStd6LinkInfo=`ls -l $LibStd6Link`
+LibStd6Name=`echo $LibStd6LinkInfo | awk '{print $11}'`					 
+LibStd6="$LibStd6Dir$LibStd6Name"
+echo $LibStd6				   
+
 mkdir "$TempDir/bin"
 mkdir "$TempDir/script"
 mkdir "$TempDir/sbin"
 
 cp "$ClientImage" "$TempDir/bin"
 cp "$ServerImage" "$TempDir/bin"
+cp "$LibStd6" "$TempDir/sbin/libstdc++.so.6"
 cp "$ProjectDir/script/installPackage.sh" "$TempDir/script"
 
 grep "Version" $ProjectDir/sourceCode/Version/HongClusterMgt_Verstion.h > /tmp/tmpvesion
