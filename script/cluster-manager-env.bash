cd8ee85eebf0c43b6268b9912816a1ec711b3172
jstar920 <51071865@qq.com>
2017-09-25 06:52:17 -0400

    modify the script

---
+++ script/cluster-manager-env.bash
@@ -0,0 +1,43 @@
+function __cmd__cluster-manager()
+{
+    local cur prev BASE_LEVEL
+    COMPREPLY=()
+    cur=${COMP_WORDS[COMP_CWORD]}
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+    
+    # Setup the base level
+    if [ $COMP_CWORD -eq 1 ]; then
+        COMPREPLY=( $(compgen -W "add delete list start stop restart trace-debug" -- $cur) )
+    # Setup the second level
+    elif [ $COMP_CWORD -eq 2 ]; then
+        case "$prev" in    
+            "add"|"delete"|"list")
+                COMPREPLY=( $(compgen -W "ui-server node-server" -- $cur) )
+                ;;
+            "start"|"stop"|"restart")
+                COMPREPLY=( $(compgen -W "client server" -- $cur) )
+                ;;
+            "trace-debug")
+                COMPREPLY=( $(compgen -W "level target" -- $cur) ) 
+                ;;
+            *)
+                ;;
+        esac
+    elif [ $COMP_CWORD -eq 3 ]; then
+        case "$prev" in
+             "ui-server"|"node-server")
+                  COMPREPLY=( $(compgen -W "ip:port" -- $cur) )
+                  ;;
+             "level")
+                  COMPREPLY=( $(compgen -W "0 1 2 3 4" -- $cur) )
+                  ;;
+             "target")
+                  COMPREPLY=( $(compgen -W "file console both" -- $cur) )
+                  ;;
+             *)
+                  ;;
+        esac
+    fi
+}
+
+complete -F __cmd__cluster-manager cluster-manager
--- script/installPackage.sh
+++ script/installPackage.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-pwd
-
 ServerIpAddressPort=""
 ServerIpAddressPortTag=false
 PackagePath=""
@@ -31,6 +29,7 @@ initInstallDir()
 help()
 {
     echo "-p path, package path, for example: -p /home/user/pcmm/"
+    echo "   package path is the bin files director or the package tar.gz file."
 }
 
 parseArgument()
@@ -72,37 +71,96 @@ parseArgument()
     done
 }
 
-
-checkPackagePath()
+checkPackageType()
 {
-    if [ ! -d "$PackagePath" ]; then
-        echo "package $PackagePath is not exsisted!"
+    if [ -f "$PackagePath" ]; then
+        TarGzExist=`echo "$PackagePath" | grep '.*HongClusterMgt\.[0-9]\.[0-9]\.tar\.gz$'`
+        if [ -z "$TarGzExist" ]; then
+            return 0
+        else
+            return 1
+        fi
+    else
+        if [ -d "$PackagePath" ]; then
+            return 2
+        fi
+    fi
+    return 0
+}
+
+checkPackagePathDir()
+{
+    local PackageFilesPath=$1
+    if [ ! -d "$PackageFilesPath" ]; then
+        echo "package $PackageFilesPath is not exsisted!"
         exit 1
     fi
-    if [ ! -d "$PackagePath/bin" ]; then
-        echo "$PackagePath/bin is not exsisted!"
+
+    if [ ! -d "$PackageFilesPath/bin" ]; then
+        echo "$PackageFilesPath/bin is not existed!"
         exit 1
     fi
-    if [ ! -d "$PackagePath/sbin" ]; then
-        echo "$PackagePath/sbin is not exsisted!"
+    
+    if [ ! -f "$PackageFilesPath/bin/ClusterNodesControl.elf" ]; then
+        echo "$PackageFilesPath/bin/ClusterNodesControl.elf is not existed!"
+        exit 1
+    fi
+
+    if [ ! -f "$PackageFilesPath/bin/ComputerNodeMonitor.elf" ]; then
+        echo "$PackageFilesPath/bin/ComputerNodeMonitor.elf is not existed!"
+        exit 1
+    fi
+
+    if [ ! -d "$PackageFilesPath/sbin" ]; then
+        echo "$PackageFilesPath/sbin is not exsisted!"
         exit 1
     fi
 }
 
+installPackageFromTarGz()
+{
+    TmpDir="/tmp/.HongInstall_qwerty_temp"
+    if [ ! -d "$TmpDir" ]; then
+        mkdir "$TmpDir"
+    fi
+    tar -xzf "$PackagePath" -C "$TmpDir"
+    installPackageFromDir "$TmpDir"
+    rm -rf "$TmpDir"
+}
+
+installPackageFromDir()
+{
+    local PackageFilesPath=$1
+    checkPackagePathDir $PackageFilesPath
+    echo "install bin!"
+    cp -r "$PackageFilesPath/bin" "$InstallDir"
+    echo "install sbin"
+    cp -r "$PackageFilesPath/sbin" "$InstallDir"
+    echo "install config"
+    UiServer="$InstallDir/config/conf.ui.server"
+    NodeServer="$InstallDir/config/conf.node.server"
+    if [ ! -f "$UiServer" ]; then
+	touch "$UiServer"
+    fi
+    if [ ! -f "$NodeServer" ]; then
+	touch "$NodeServer"
+    fi
+}
 
 installPackage()
 {
-    echo "$InstallDir"
-    cp -r "$PackagePath/bin" "$InstallDir"
-    cp -r "$PackagePath/sbin" "$InstallDir"
-	UiServer="$InstallDir/config/conf.ui.server"
-	NodeServer="$InstallDir/config/conf.node.server"
-	if [ ! -f "$UiServer" ]; then
-		touch "$UiServer"
+    checkPackageType
+    Type=$?
+    if [ $Type -eq 1 ]; then
+        installPackageFromTarGz $PackagePath
+    elif [ $Type -eq 2 ]; then
+        installPackageFromDir $PackagePath
+    else
+        echo "error usage command!"
+        help
+        exit 1
     fi
-	if [ ! -f "$NodeServer" ]; then
-		touch "$NodeServer"
-    fi
+    echo "installation completed"
 }
 
 parseArgument $@
